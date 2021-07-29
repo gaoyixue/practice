f04f038838e8d0fcc1f2a4dc4d31f6ea80e16b96
gaoyixue <yixuegao@126.com>
2021-07-29 22:11:11 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+void func1(){
+	printf("hahaha");
+	printf("hahaha");
+	printf("hahaha");
+	printf("hahaha");
+	printf("hahaha");
+	printf("hahaha");
+}
+
+void func2(){
+	printf("ha2");
+}
+
+voif func3(){
+	printf("hahah3");
+	printf("hahah3");
+	printf("hahah3");
+	printf("hahah3");
+	printf("hahah3");
+	printf("hahah3");
+}
