d941b7a943378ad1c4234b9c4b74cd0a50df4110
gaoyixue <yixuegao@126.com>
2021-07-29 22:22:45 +0800

    second commit

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 void func1(){
-	printf("hahaha");
-	printf("hahaha");
-	printf("hahaha");
-	printf("hahaha");
-	printf("hahaha");
-	printf("hahaha");
+	printf("ha1");
+	printf("ha1");
+	printf("ha1");
+	printf("ha1");
+	printf("ha1");
 }
 
 void func2(){
 	printf("ha2");
+	printf("ha2");
+	printf("ha2");
+	printf("ha2");
+	printf("ha2");
+	printf("ha2");
 }
 
 voif func3(){
-	printf("hahah3");
-	printf("hahah3");
-	printf("hahah3");
-	printf("hahah3");
-	printf("hahah3");
-	printf("hahah3");
+	printf("ha3");
+	printf("ha3");
+	printf("ha3");
+	printf("ha3");
+	printf("ha3");
+	printf("ha3");
 }
